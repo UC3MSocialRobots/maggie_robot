6667103ad620cc525a998ce399a248046fb81b1e
Raúl Pérula-Martínez <raules@gmail.com>
2015-05-07 09:21:29 +0200

    updated udev rules script

--- maggie_bringup/scripts/install.bash
+++ maggie_bringup/scripts/install.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-### copy udev rules
+# copy udev rules
 
 echo "Copying udev rules..."
 
 cd `rospack find maggie_bringup`/config/
 sudo cp 99-maggie.rules /etc/udev/rules.d/
+
+# restart and reload udev rules
+
+echo "Restarting udev rules..."
+
+sudo service udev restart
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo "FINISH!"
