e0cb72d41ab807084b7f6df1e76861358d784fad
Raul Perula-Martinez <raul.perula@uc3m.es>
2015-05-26 13:33:15 +0200

    updated instal scrip

--- maggie_bringup/scripts/install.bash
+++ maggie_bringup/scripts/install.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Config permissions
+
 # add 'user' to the groups 'dialout', and 'video'
 
 sudo usermod -a -G dialout, video $USER
@@ -20,3 +22,10 @@ sudo udevadm control --reload-rules
 sudo udevadm trigger
 
 echo "FINISH!"
+
+## (optional) Config IR permissions
+
+file=`rospack find maggie_ir_drivers`/scripts/config_driver.sh
+if [ -e "$file" ]; then
+    sh $file
+fi 
